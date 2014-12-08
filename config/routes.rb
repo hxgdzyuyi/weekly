845c90a12978ffb4dd60c2c2cf34a5f68c75ab57
hxgdzyuyi <hxgdzyuyi@gmail.com>
2014-12-08 15:37:19 +0800

    添加 adminUser 用来管理程序

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'admin/admin_users#index'
+  devise_for :admin_users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +54,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  namespace :admin do
+    resources :admin_users
+  end
 end
