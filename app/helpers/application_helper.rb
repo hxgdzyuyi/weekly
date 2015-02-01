cc6865fa8186ceee0f627ca3b7e6584e3d1b158c
kyon <kyon0304@gmail.com>
2015-02-01 14:20:02 +0800

    use gravatar in _nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,10 @@ module ApplicationHelper
     HTML
     html
   end
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest( user.email.downcase )
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag( gravatar_url, alt: user.email, class: "gravatar" )
+  end
 end
