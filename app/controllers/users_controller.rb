d5e060499a8ef096ee8b97ceff3ca5e9e1054ed0
hxgdzyuyi <hxgdzyuyi@gmail.com>
2015-02-25 23:00:41 +0800

    添加个人 Profile 页

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:collectives]
+  before_action :set_user, only: [:collectives, :show]
 
   def collectives
     @collectives = @user.collectives
   end
 
+  def show
+    @collectives = @user.collectives[0...3]
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
